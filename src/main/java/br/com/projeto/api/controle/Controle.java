5208a2ec945522e6a5263bb3550c2679874d8263
Daniel Rodrigues Alves <drodriguesalves2@gmail.com>
2023-05-20 22:15:17 -0300

    Spring Boot Api - back

---
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -0,0 +1,33 @@
+package br.com.projeto.api.controle;
+
+import br.com.projeto.api.modelo.Cliente;
+import br.com.projeto.api.repositorio.Repositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class Controle {
+    @Autowired
+    private Repositorio repositorio;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c){
+        return repositorio.save(c);
+    }
+
+    @GetMapping("/")
+    public Iterable<Cliente> selecionar(){
+        return repositorio.findAll();
+    }
+
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente c){
+        return repositorio.save(c);
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void remover(@PathVariable long codigo){
+        repositorio.deleteById(codigo);
+    }
+}
